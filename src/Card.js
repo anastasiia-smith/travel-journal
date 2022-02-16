8f6853e17f3fb9c76b61bacc59e599b60fe55ad1
Anastasiia Smith <anastasiiasmithdev@gmail.com>
2022-02-15 20:08:17 -0500

    Edited classes for better BEM

--- src/Card.js
+++ src/Card.js
@@ -7,14 +7,14 @@ export default function Main(props) {
         <img src={props.imageUrl} alt={`${props.title}, ${props.location}`}/>
       </div>
       <div className="card__content">
-        <div className="card__content-location">
+        <div className="card__location">
           <img src={pin} alt="Map pin" />
           <span>{props.location}</span>
           <a href={props.googleMapsUrl}>View on Google Maps</a>
         </div>
-        <h3 className="card__content-header">{props.title}</h3>
-        <div className="card__content-date">{props.startDate} - {props.endDate}</div>
-        <p className="card__content-descripion">{props.description}</p>
+        <h3 className="card__header">{props.title}</h3>
+        <div className="card__date">{props.startDate} - {props.endDate}</div>
+        <p className="card__descripion">{props.description}</p>
       </div>
     </div>
   )
